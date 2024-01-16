ecbdfbff97604f53f8d6814d7bc5f81c88616673
Ellen Spertus <ellen.spertus@gmail.com>
2024-01-15 17:46:57 -0800

    initial commit

---
+++ submissions/buggy/EmptyLOS.java
@@ -0,0 +1,22 @@
+package student;
+
+/**
+ * An immutable empty list of Strings.
+ */
+public class EmptyLOS implements ILOS {
+    /**
+     * Constructs an empty list of strings.
+     */
+    public EmptyLOS() {
+    }
+
+    @Override
+    public int size() {
+        return Integer.MIN_VALUE; // intentional bug
+    }
+
+    @Override
+    public String concat() {
+        return null; // intentional bug
+    }
+}
